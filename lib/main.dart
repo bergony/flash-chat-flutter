595cb795482cc6cc8679ea9ea450d7da769ed666
Bergony <Bergony@hotmail.com>
2021-05-24 18:10:17 -0300

    Add Stack route. close #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
-import 'package:flutter/material.dart';
-import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
-import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
 
 void main() => runApp(FlashChat());
 
@@ -12,10 +16,17 @@ class FlashChat extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
+          // ignore: deprecated_member_use
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        'login_screenn': (context) => LoginScreen(),
+        'rigistration_screenn': (context) => RegistrationScreen(),
+        'chat_screenn': (context) => ChatScreen(),
+      },
     );
   }
 }
